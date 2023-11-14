e58ec39cd6c9268f693b717a0d4edbc4b7bc5265
aliashtab77 <109904632+aliashtab77@users.noreply.github.com>
2023-11-15 01:53:12 +0330

    جلسه سوم

---
+++ 03/numbertypes.py
@@ -0,0 +1,11 @@
+a = 5  
+print("The type of a", type(a))  
+print(" a is a int number", isinstance(a,int))
+
+b = 40.5  
+print("The type of b", type(b))  
+print(" b is a float number", isinstance(b,float))
+
+c = 1+3j  
+print("The type of c", type(c))  
+print(" c is a complex number", isinstance(c,complex))  
---
+++ 03/strx1.py
@@ -0,0 +1,8 @@
+st = "string using double quotes"  
+bf = 'hello stark'
+print(st)  
+
+s = '''A multiline 
+string'''  
+
+print(s) 
